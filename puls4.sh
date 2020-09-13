2ba01fc0ce6b8cf6094c1f8a9fb31f8b23e89be4
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-09-14 06:39:58 +0700

    Puls4 gratis

---
+++ puls4.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+figlet PULSA FREE
+
+echo "{×}==================================={×}"
+echo "Creator: Hacker20"
+echo "-----------------------------------------"
+echo "Team   : 1.ELANG CYBER ARMY"
+echo "         2.FAMILY QUICK TRIANGLE"
+echo "-----------------------------------------"
+echo "Thanks : > SK4T3R AND TEAM"
+echo "         > The Coward"
+echo "         > IPULL=TUAN SEMPAK=TUAN G4L4U"
+echo "{×}==================================={×}"
+echo ""
+echo "Oke cuk ga usah banyak bacot, langsung pilih mau apa"
+echo "___________________________________________________"
+echo ""
+echo "1.> ISI PULSA SENDIRI    "$red
+echo ""
+echo "2.> ISI PULSA TEMAN      "$green
+echo "___________________________________________________"
+
+echo ""
+echo "\033[34;1m
+99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m Pilih pilih aja gk usah.komen!\033[32;1m"
+read -p " PILIHAN=> " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet ISI PULSA | lolcat
+echo "\033[34;1msabar asw"
+echo ""
+sleep 1
+
+echo "mau isi pulsa berapa.?"
+read -p "(ex:5000,10000,20000,50000,100000 dan seterusnya)=> " pulsa
+echo ""
+echo ""
+echo "nomor lu"
+read -p "nomor lu eaa =>" nomor
+echo "Sabar asw lagi ngirim"
+echo ""
+echo ""
+echo "jumlah pulsa :" $pulsa|
+echo "ke nomer :" $nomor
+echo ""
+echo ""
+sleep 2
+clear
+figlet LOADING
+sleep 15
+echo ""
+echo "Wkwkwkwkwkwkwkwkwkwkwkwkwkwkw"
+echo ""
+sleep 2
+echo "mau aja lu gw kerjain. makan tuh pulsa gratis"
+echo "makanya modal dong, jgn cuma mau yg gratisan mulu"
+echo ""
+sleep 2
+echo "ya uda makasih eaa uda coba tools gua"
+echo "pesan gw"
+sleep 2
+echo "Jangan Lupa Subscribe Channel Gw"
+echo "Channel : Sandi Yt"
+sleep 2
+figlet B . Y . E
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet SEND PULSA
+echo "\033[34;1msabar asw"
+echo ""
+sleep 1
+
+echo "mau isiin pulsa temen kamu eaa"
+echo "mau send pulsa brp.?"
+read -p "(ex:5000,10000,20000,50000,100000dan seterusnya)=> " a
+echo ""
+echo ""
+echo "OPERATOR"
+read -p "(ex:Indosat,3,Telkomsel,Axis,XL, dll=> " b
+echo ""
+echo ""
+echo "nomor tujuan"
+read -p "=>" c
+echo ""
+echo ""
+sleep 1
+echo "nomor target : " $c
+echo "OPERATOR     : " $b
+echo "Jumlah pulsa : " $a
+sleep 2
+clear
+figlet LOADING
+sleep 15
+echo ""
+echo "Wkwkwkwkwkwkwkwkwkwkwkwkwkwkw"
+echo ""
+sleep 2
+echo "mau aja lu gw kerjain. makan tuh pulsa gratis"
+echo "makanya modal dong, jgn cuma mau yg gratisan mulu"
+echo ""
+sleep 2
+echo "ya uda makasih eaa uda coba tools gua"
+echo "pesan gw"
+sleep 2
+echo "Jangan Lupa Subscribe Channel Gw"
+echo "Channel : Sandi Yt"
+sleep 2
+figlet B . Y . E
+echo $green"=============================="
+echo $red"YouTube : Sandi Yt"
+echo $red"Contact : 6287778506999"
+echo $green"=============================="
+fi
